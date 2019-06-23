fc9c3878f324593e5abbb2bfb347b5d303e80593
Velissel <william.wenyan@gmail.com>
2019-06-23 21:34:29 +1000

    fixed tests for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
+import { createStore } from './store';
+import { Provider } from 'react-redux';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App.js', () => {
+  let store;
+  beforeEach(() => {
+    store = createStore().store;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
